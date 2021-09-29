2f69f298ec181f39d9dbaf2238fbf104c135d159
Melvin Omega <melvinomega151@gmail.com>
2021-09-29 08:40:15 +0300

    add token

--- app.py
+++ app.py
@@ -27,6 +27,28 @@ class User(db.Model):
     password = db.Column(db.String(100))
     admin = db.Column(db.Boolean)
 
+## User Token
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+
+        if 'x-access-token' in request.headers:
+            token=request.headers['x-access-token']
+
+        if not token:
+            return jsonify({'message' : 'Token is missing for authentication'}), 401
+
+
+        # if token is there 
+        try:
+            data =jwt(token, app.config['SECRET_KEY']) 
+            current_user =User.query.filter_by(public_id=data['public_id']).first()
+        except:
+            return jsonify({'message': 'Token is invalid!'}), 401
+
+        return  f(current_user, *args , **kwargs)  
+    return decorated
 
 
 
